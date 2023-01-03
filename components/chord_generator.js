ec240f437f1f7b2fb2c0e4aff8a4a40b385a376b
taxpurposes <danjzych@gmail.com>
2023-01-02 22:01:11 -0600

    This commit has the basic structure of web page and project. Added a chord generator file to import into main.js for speration of concerns. Chord generator will house Object of all different keys, and then generate the possible chord progressions. Or should i put the keys in a JSON?

---
+++ components/chord_generator.js
@@ -0,0 +1,13 @@
+const MusicalKeys = {
+    'Ab': {
+        'NotesInKey': [
+        'Ab', 'Bb', 'C', 'Db', 'Eb', 'F', 'G'
+    ]},
+    'A': [
+
+    ]
+}
+
+export default MusicalKeys;
+
+console.log(MusicalKeys['Ab']['NotesInKey'])
---
+++ main.js
@@ -0,0 +1,12 @@
+import MusicalKeys from './components/chord_generator.js';
+
+/*$('document').ready(() => {
+
+    //general animations
+
+    ///////////////////////
+    /// chord generator ///
+    ///////////////////////
+
+
+});*/
